8285da546fdb63e7f32a3204b3d5066ae97500c2
Omar Hammad <hammado92@gmail.com>
2024-04-25 13:57:38 +0300

    nunjucks-demo

---
+++ nunjucks-demo-starter/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+// Dummy Data (will get these from the DB later)
+const users = [
+    { id: "a", name: "ahmad", isAdmin: false },
+    { id: "b", name: "bilal", isAdmin: true }
+]
+
+const tasks = [
+    { id: 1, name: "Do Phase 5", due: new Date(), userId: "a" },
+    { id: 2, name: "Do HW 3", due: new Date("2024-04-27"), userId: "b" }
+]
+
+const user = users[1]
+
+//serve public folder
+app.use(app.express("public"))
+
+const PORT = 3000
+app.listen(PORT, function () {
+    console.log(`server is listeneing to port ${PORT}`)
+})
